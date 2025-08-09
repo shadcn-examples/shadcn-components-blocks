7247771ad2ff8e66dd3b054793be6bccbd0ea3d0
toby <hello@tobybelhome.com>
2025-08-09 21:30:15 +0300

    GoogleAnalyticsInit

--- lib/ga.ts
+++ lib/ga.ts
@@ -1,18 +1,12 @@
 "use client";
 
-import { useEffect } from "react";
 import ReactGA from "react-ga4";
+
+import { useEffect } from "react";
 
 export default function GoogleAnalyticsInit() {
   useEffect(() => {
-    const GA_KEY = process.env.NEXT_PUBLIC_GA_KEY;
-
-    if (!GA_KEY) {
-      console.error("Google Analytics key not provided.");
-      return;
-    }
-
-    ReactGA.initialize(GA_KEY);
+    ReactGA.initialize(`${process.env.NEXT_PUBLIC_GA_KEY}`);
     ReactGA.send("pageview");
   });
 
